3d7d0681675d42d1a2714a99db7e677a880d1ac0
ralf1albrecht1-byte <ralf1albrecht1@gmail.com>
2026-05-04 07:09:06 +0200

    Update page.tsx

--- app/(app)/einstellungen/page.tsx
+++ app/(app)/einstellungen/page.tsx
@@ -1133,14 +1133,13 @@ const showLogo = previewLogoUrl.length > 0;
           </div>
 
 {showLogo ? (
- <img
+<img
   src={previewLogoUrl}
   alt=""
   className="h-14 max-w-[140px] object-contain"
   crossOrigin="anonymous"
   referrerPolicy="no-referrer"
-/>  referrerPolicy="no-referrer"
-/>) : (
+/>
   <div className="flex flex-col items-end gap-1 text-right">
     <div className="h-5 w-14 rounded bg-gray-200/90" />
     <p className="text-[8px] text-gray-500 max-w-[110px] truncate">
